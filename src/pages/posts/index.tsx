c0a9f5625c81b6930effa6c839f89f4d74bc80fc
Suboch. S <sema.chel12@gmail.com>
2024-03-27 20:17:23 +0300

    posts and Createpost

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,18 @@
 import React from "react"
+import { useGetAllPostsQuery } from "../../app/services/postsApi"
+import CreatePost from "../../components/create-post"
 
 const Posts = () => {
-  return <div>Posts</div>
+  //сразу идёт запрос
+  const { data } = useGetAllPostsQuery()
+
+  return (
+    <>
+      <div className="mb-10 w-full">
+        <CreatePost />
+      </div>
+    </>
+  )
 }
 
 export default Posts
